da4439075be9faeb1d4fc31905ad36a050b8f4a6
Sage Dragon <sagethunderdragon@gmail.com>
2019-09-10 11:24:24 -0400

    Got the bulk of the game working. Pick random word, make word array, push to html, etc...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,91 @@
-var answers = ["kitten", "unicorn", "rainbow", "butterfly"];
+var answers = ["kitten", "unicorn", "otter", "butterfly", "hedgehog", "puppy", "griffin", "phoenix"]
 
-var theWord = answers[Math.floor(Math.random() * answers.length)];
+var randomWord = "";
+var theWordArray = []
+var blanks = 0;
+var blanksAndCorrect = [];
+var wrongLetter = [];
 
-var theWordArray =[];
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 9;
 
-for (var i = 0; i < theWord.length; i++){
-    theWordArray[i] = "_";
+
+
+function Game() {
+    randomWord = answers[Math.floor(Math.random() * answers.length)];
+
+    theWordArray = randomWord.split("");
+
+    blanks = theWordArray.length;
+
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
+
+    document.getElementById("gameWord").innerHTML = "  " + blanksAndCorrect.join("  ");
+
+    console.log(randomWord);
+    console.log(theWordArray)
+    console.log(blanks)
+    console.log(blanksAndCorrect)
 }
 
-var lettersRemain = theWord.length;
+function reset() {
+    guessesRemaining = 9;
+    wrongLetter = [];
+    blanksAndCorrect = [];
+    Game()
+}
+
+function checkLetters(letter) {
+    var letterInWord = false;
+    for (var i = 0; i < blanks; i++) {
+        if (randomWord[i] == letter) {
+            letterInWord = true;
+        }
+    }
+    if (letterInWord) {
+        for (var i = 0; i < blanks; i++) {
+            if (randomWord[i] == letter) {
+                blanksAndCorrect[i] = letter;
+            }
+        }
+    }
+    else {
+        wrongLetter.push(letter);
+        guessesRemaining--;
+    }
+    console.log(blanksAndCorrect);
+}
+
+function complete() {
+    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
+
+    if (theWordArray.toString() == blanksAndCorrect.toString()) {
+        wins++;
+        reset()
+        document.getElementById("winstracker").innerHTML = " " + wins;
+
+    } else if (guessesRemaining === 0) {
+        losses++;
+        reset()
+        document.getElementById("losstracker").innerHTML = " " + losses;
+    }
+    document.getElementById("gameWord").innerHTML = "  " + blanksAndCorrect.join(" ");
+    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
+}
+
+Game()
+
+document.onkeyup = function (event) {
+    var guesses = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(guesses);
+    complete();
+    console.log(guesses);
+
+    document.getElementById("gameLetters").innerHTML = "  " + wrongLetter.join(" ");
+}
+
+
+
